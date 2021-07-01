84b33b4eb427b6aba2311648e15387fba80d8896
MarlonGregorio <43593484+MarlonGregorio@users.noreply.github.com>
2021-07-01 15:59:53 -0700

    Add files via upload

---
+++ Audio/MusicManager.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+    public bool musicCanPlay;
+    public VolumeManager theVM;
+
+
+    public AudioSource[] musicTracks;
+
+    public int currentTrack;
+
+    private int currentDelay;
+    private int toSwitch;
+    private int counter;
+    private bool ignite;
+
+
+	void Start () {
+		
+	}
+	
+
+
+	void Update () {
+
+
+        if(ignite)
+        {
+            counter++;
+            if(counter==currentDelay)
+            {
+                musicCanPlay = false;
+                
+                theVM.slowlyHigher();
+                
+            }
+
+            if(counter == currentDelay + 30)
+            {
+                musicCanPlay = true;
+                SwitchTrack(toSwitch);
+                ignite = false;
+                counter = 0;
+            }
+        }
+
+
+
+
+
+
+		if(musicCanPlay)
+        {
+            if(!musicTracks[currentTrack].isPlaying)
+            {
+                musicTracks[currentTrack].Play();
+            }
+        }
+        else
+        {
+            musicTracks[currentTrack].Stop();
+        }
+	}
+
+    public void SwitchTrack(int newTrack)
+    {
+        //if (newTrack != currentTrack)
+       // {
+            musicTracks[currentTrack].Stop();
+            currentTrack = newTrack;
+            musicTracks[currentTrack].Play();
+       // }
+               
+    }
+
+    public void slowSwitch(int newTrack, int delay)
+    {
+        currentDelay = delay;
+        toSwitch = newTrack;
+        ignite = true;
+
+        theVM.slowlyLower();
+
+    }
+}
---
+++ Audio/SFXManager.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXManager : MonoBehaviour {
+
+ 
+    public AudioSource menuClick;
+
+    public AudioSource pause;
+
+    public AudioSource unPause;
+
+    public AudioSource coin;
+
+    public AudioSource hitblock;
+
+    public AudioSource jump;
+
+    public AudioSource stomp;
+
+    public AudioSource powerUp;
+
+    public AudioSource scoreCount;
+
+    public AudioSource ding;
+
+    public AudioSource ding2;
+
+    public AudioSource ding3;
+
+    public AudioSource endLevel;
+
+    public AudioSource death;
+
+    public AudioSource punch;
+
+    public AudioSource blast;
+
+    public AudioSource checkPoint;
+
+
+
+
+    public void soundMenuClick()
+    {
+        menuClick.Play();
+    }
+
+    public void PlaySound(string x)
+    {
+        if (x == "MenuClick")
+        {
+            menuClick.Play();
+        }
+        if (x == "Coin")
+        {
+            coin.Play();
+        }
+        if (x == "HitBlock")
+        {
+            hitblock.Play();
+        }
+
+        if (x == "Jump")
+        {
+            jump.Play();
+        }
+
+        if (x == "Stomp")
+        {
+            stomp.Play();
+        }  
+
+        if (x == "PowerUp")
+        {
+            powerUp.Play();
+        }
+
+        if (x == "Pause")
+        {
+            pause.Play();
+        }
+        if (x == "UnPause")
+        {
+            unPause.Play();
+        }
+        if (x == "ScoreCount")
+        {
+            scoreCount.Play();
+        }
+        if (x == "Ding1")
+        {
+            ding.Play();
+        }
+        if (x == "Ding2")
+        {
+            ding2.Play();
+        }
+        if (x == "Ding3")
+        {
+            ding3.Play();
+        }
+        if (x == "EndLevel")
+        {
+            endLevel.Play();
+        }
+
+        if (x == "Death")
+        {
+            death.Play();
+        }
+
+        if (x == "Punch")
+        {
+            punch.Play();
+        }
+
+        if (x == "Blast")
+        {
+            blast.Play();
+        }
+
+
+        if (x=="StopScore")
+        {
+            scoreCount.Stop();
+        }
+
+        if (x == "CheckPoint")
+        {
+            checkPoint.Play();
+        }
+    }
+}
